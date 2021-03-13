5a636ed147cbfbdc203d53fc8caed1ce8a103127
AmandaIn <amandainacio@id.uff.br>
2021-03-13 20:05:30 -0300

    inclusão do re para tratar as informações

--- ontoscrapy_tcc/spiders/wiki_scraping.py
+++ ontoscrapy_tcc/spiders/wiki_scraping.py
@@ -1,7 +1,6 @@
-import lxml
 import scrapy
 import re
-from lxml import etree
+
 
 
 class WikiScraping(scrapy.Spider):
@@ -16,49 +15,13 @@ class WikiScraping(scrapy.Spider):
         y = "vida"
 
         vida = "developer"
-        for x in range(len(tr)):
+        for n in range(len(tr)):
 
-            teste = tr[x].extract()
-            ', '.join(teste)
-            resultado = re.search(vida, teste)
-            #resultado = compara(teste, vida)
+            line = tr[n].extract()
+            ', '.join(line)
+            resultado = re.search(vida, line)
+
             if resultado.group(0) == "developer":
+                yield dict(developer=re.sub('<[^>]+?>', '', tr[n].extract()))
                 print(resultado.group(0))
-            #print(teste)
-            # yield dict(info=tr[x].extract())
 
-            #if compara(teste, "developer") == "developer":
-               # print(teste)
-
-            # limpar = tr[x].extract()
-            # teste = response.xpath('//table[@class="infobox vevent"]//tr//th//text()')[x].extract()
-            # print(teste)
-            # if response.xpath('//table[@class="infobox vevent"]//tr//th//text()')[x].extract() == 'Developer(s)':
-            # yield dict(developer=response.xpath('//table[@class="infobox vevent"]//tr//th.//td//text()')[x].extract())
-            # if response.xpath('//table[@class="infobox vevent"]//tr//th//text()')[x].extract() == 'Stable release':
-            # yield dict(stable_release=tr[x].extract())
-            # if response.xpath('//table[@class="infobox vevent"]//tr//th//text()')[x].extract() == 'Written in':
-            # yield dict(written_in=tr[x].extract())
-
-            # yield dict(info=limpar[x].extract())
-            # yield dict(info=tr[x].extract())
-            # if (limpar.xpath)
-            # document = lxml.html.document_fromstring(limpar)
-            # print(raw_text = document.text_content())
-
-
-def compara(lista, palavra):
-
-    for n in range(len(lista)):
-        comparar = lista[n]
-        resultado = re.search(palavra, comparar)
-        if resultado:
-            return resultado.group(0)
-            """ for x in tr:    
-    
-                    # print(x.extract())
-                     yield dict(info=response.xpath('//table[@class="infobox vevent"]//tr[1]//text()').extract())
-                     #yield dict(info=x.extract())
-                     
-                     
-            """
