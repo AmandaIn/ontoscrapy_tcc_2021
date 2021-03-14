83ba3df20b1487710a6da755bafc447e439daa75
AmandaIn <amandainacio@id.uff.br>
2021-03-14 01:41:29 -0300

    inclusão de atributos de paginas diferentes do clustal e maft

--- ontoscrapy_tcc/spiders/wiki_scraping.py
+++ ontoscrapy_tcc/spiders/wiki_scraping.py
@@ -6,21 +6,23 @@ class WikiScraping(scrapy.Spider):
     name = "inf_program"
 
     start_urls = ["https://en.wikipedia.org/wiki/Clustal#ClustalW",
-                  "https://en.wikipedia.org/wiki/MAFFT"
+                  "https://en.wikipedia.org/wiki/MAFFT",
+                  "https://en.wikipedia.org/wiki/MUSCLE_(alignment_software)"
                   ]
 
     def parse(self, response):
-        tr = response.xpath('//table[@class="infobox vevent"]//tr')
-        y = "vida"
 
-        attribute = ['release', 'developer']
+        tr = response.xpath('//table[@class="infobox vevent"]//tr')
+
+        attribute = ['release', 'developer', 'system', 'categories', 'license', 'website', 'written', 'author']
+
         for n in range(len(tr)):
 
             line = tr[n].extract()
             ', '.join(line)
 
             for m in range(len(attribute)):
-                result_line = re.search(attribute[m], line)
+                result_line = re.search(attribute[m], line, re.IGNORECASE)
 
                 if result_line and (attribute[m] == "developer"):
                     yield dict(developer=re.sub('<[^>]+?>', '', tr[n].extract()))
@@ -28,23 +30,25 @@ class WikiScraping(scrapy.Spider):
                 if result_line and (attribute[m] == "release"):
                     yield dict(stable_release=re.sub('<[^>]+?>', '', tr[n].extract()))
 
+                if result_line and (attribute[m] == "system"):
+                    yield dict(operating_system=re.sub('<[^>]+?>', '', tr[n].extract()))
+
+                if result_line and (attribute[m] == "categories"):
+                    yield dict(category=re.sub('<[^>]+?>', '', tr[n].extract()))
+
+                if result_line and (attribute[m] == "license"):
+                    yield dict(license=re.sub('<[^>]+?>', '', tr[n].extract()))
+
+                if result_line and (attribute[m] == "website"):
+                    yield dict(website=re.sub('<[^>]+?>', '', tr[n].extract()))
+
+                if result_line and (attribute[m] == "written"):
+                    yield dict(written_in=re.sub('<[^>]+?>', '', tr[n].extract()))
+
+                if result_line and (attribute[m] == "author"):
+                    yield dict(original_author=re.sub('<[^>]+?>', '', tr[n].extract()))
 
 
 
-
-
-
-            #print(result_line.group(0))
-
-            #print(result_line)
-            #result_line2 = re.search("system", line)
-
-            # if result_line.group(0) == "developer":
-              # yield dict(developer=re.sub('<[^>]+?>', '', tr[n].extract()))
-              # print(result_line.group(0))
-
-            #f result_line2.group(0) == "system":
-               # yield dict(sistema=re.sub('<[^>]+?>', '', tr[n].extract()))
-                #print(result_line2.group(0))
-
+      #  yield dict(description=re.sub('<[^>]+?>', '', response.xpath('//p').extract_first()))
 
