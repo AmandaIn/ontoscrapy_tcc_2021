de4ccb897b2450565b397f7ec91a68cf6787a724
AmandaIn <amandainacio@id.uff.br>
2021-03-14 22:35:39 -0300

    inclusão do name e de mais alguns atributos

--- ontoscrapy_tcc/spiders/wiki_scraping.py
+++ ontoscrapy_tcc/spiders/wiki_scraping.py
@@ -7,14 +7,18 @@ class WikiScraping(scrapy.Spider):
 
     start_urls = ["https://en.wikipedia.org/wiki/Clustal#ClustalW",
                   "https://en.wikipedia.org/wiki/MAFFT",
-                  "https://en.wikipedia.org/wiki/MUSCLE_(alignment_software)"
+                  "https://en.wikipedia.org/wiki/MUSCLE_(alignment_software)",
+                  "https://en.wikipedia.org/wiki/JAligner"
                   ]
 
     def parse(self, response):
 
+        yield dict(name_program=response.xpath('//title//text()').extract_first())
+        yield dict(description=re.sub('<[^>]+?>', '', response.xpath('//p').extract_first()))
+
         tr = response.xpath('//table[@class="infobox vevent"]//tr')
 
-        attribute = ['release', 'developer', 'system', 'categories', 'license', 'website', 'written', 'author']
+        attribute = ['author', 'developer', 'release', 'repository',  'system', 'platform', 'available', 'categories', 'license', 'website', 'written']
 
         for n in range(len(tr)):
 
@@ -24,14 +28,26 @@ class WikiScraping(scrapy.Spider):
             for m in range(len(attribute)):
                 result_line = re.search(attribute[m], line, re.IGNORECASE)
 
+                if result_line and (attribute[m] == "author"):
+                    yield dict(original_author=re.sub('<[^>]+?>', '', tr[n].extract()))
+
                 if result_line and (attribute[m] == "developer"):
                     yield dict(developer=re.sub('<[^>]+?>', '', tr[n].extract()))
 
                 if result_line and (attribute[m] == "release"):
-                    yield dict(stable_release=re.sub('<[^>]+?>', '', tr[n].extract()))
+                    yield dict(release=re.sub('<[^>]+?>', '', tr[n].extract()))
+
+                if result_line and (attribute[m] == "repository"):
+                    yield dict(repository=re.sub('<[^>]+?>', '', tr[n].extract()))
 
                 if result_line and (attribute[m] == "system"):
                     yield dict(operating_system=re.sub('<[^>]+?>', '', tr[n].extract()))
+
+                if result_line and (attribute[m] == "platform"):
+                    yield dict(platform=re.sub('<[^>]+?>', '', tr[n].extract()))
+
+                if result_line and (attribute[m] == "available"):
+                    yield dict(available_in=re.sub('<[^>]+?>', '', tr[n].extract()))
 
                 if result_line and (attribute[m] == "categories"):
                     yield dict(category=re.sub('<[^>]+?>', '', tr[n].extract()))
@@ -45,10 +61,9 @@ class WikiScraping(scrapy.Spider):
                 if result_line and (attribute[m] == "written"):
                     yield dict(written_in=re.sub('<[^>]+?>', '', tr[n].extract()))
 
-                if result_line and (attribute[m] == "author"):
-                    yield dict(original_author=re.sub('<[^>]+?>', '', tr[n].extract()))
 
 
 
-      #  yield dict(description=re.sub('<[^>]+?>', '', response.xpath('//p').extract_first()))
+
+
 
